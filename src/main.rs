05582338a6e30976bd61be8cd765d6c399811c5f
zraktvor <zraktvor@protonmail.com>
2023-01-29 17:40:45 +0100

    Update to bevy 0.9.1

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
-use bevy::{
-    prelude::*,
-};
 use bevy::gltf::{Gltf, GltfMesh};
+use bevy::prelude::*;
 
 fn main() {
     App::new()
@@ -11,12 +9,10 @@ fn main() {
         .run();
 }
 
+#[derive(Resource)]
 struct MyAssetPack(Handle<Gltf>);
 
-fn load_gltf(
-    mut commands: Commands,
-    ass: Res<AssetServer>,
-) {
+fn load_gltf(mut commands: Commands, ass: Res<AssetServer>) {
     let gltf = ass.load("Fox.glb");
     commands.insert_resource(MyAssetPack(gltf));
 }
@@ -29,20 +25,20 @@ fn setup(
     assets_gltf: Res<Assets<Gltf>>,
     assets_gltfmesh: Res<Assets<GltfMesh>>,
 ) {
-    if !ev_asset.iter().any(|it| matches!(it, AssetEvent::Created{..})) {
+    if !ev_asset.iter().any(|it| matches!(it, AssetEvent::Created { .. })) {
         return;
     }
 
     let gltf = assets_gltf.get(&my.0).unwrap();
     let fox_gltf_mesh = assets_gltfmesh.get(&gltf.meshes[0]).unwrap();
 
-    commands.spawn_bundle(PbrBundle {
+    commands.spawn(PbrBundle {
         mesh: fox_gltf_mesh.primitives[0].mesh.clone(),
         ..default()
     });
 
     // light
-    commands.spawn_bundle(PointLightBundle {
+    commands.spawn(PointLightBundle {
         point_light: PointLight {
             intensity: 1500.0,
             ..Default::default()
@@ -52,7 +48,7 @@ fn setup(
     });
 
     // camera
-    commands.spawn_bundle(Camera3dBundle {
+    commands.spawn(Camera3dBundle {
         transform: Transform::from_xyz(5.0, 10.0, 15.0).looking_at(Vec3::from((0.0, 0.0, 0.0)), Vec3::Y),
         ..Default::default()
     });
